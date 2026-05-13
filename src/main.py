0aedad06e03bc10afc59b938b94447229d6f8463
hamedkhodami <hamedkhodami977@gmail.com>
2026-05-13 17:36:08 +0330

    Project Base(6-1)

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi_swagger import patch_fastapi
+from contextlib import asynccontextmanager
+from tasks.routes import router as task_router
+
+tags_metadata = [
+    {"name": "Tasks",
+     "description": "Operations related with tasks",
+     "externalDocs": {
+         "description": "Task description",
+         "url": "/tasks"
+     }
+     },
+]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application started")
+    yield
+    print("Application finished")
+
+app = FastAPI(title="Todo Applications",description="this is section for description"
+              ,version="0,0,1",terms_of_service="http;//example.com/terms"
+              ,contact={
+                "name": "Hamed Khodami",
+                "email": "khodamihamed77@gmail.com",
+               },license_info={"name":"MIT"},lifespan=lifespan, docs_url=None,
+              redoc_url=None, swagger_ui_oauth2_redirect_url=None)
+patch_fastapi(app)
+
+app.include_router(task_router)
